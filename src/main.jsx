73754969a4ef436480f80649746a3b96cd25d316
rayae19 <darkxop44@gmail.com>
2026-04-10 08:42:29 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "../soundbound.jsx";
 
+const container = document.getElementById("root");
+
+if (!container) {
+  throw new Error(
+    "[main.jsx] Could not find #root element. " +
+    "Make sure index.html contains <div id=\"root\"></div>."
+  );
+}
+
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
